61c85a31f12ca71789e05658dffe6ea063e58f06
Tobi Abolade <tobiabolade@gmail.com>
2022-02-28 16:26:04 +0000

    week4

--- week4/script.js
+++ week4/script.js
@@ -1,9 +1,9 @@
 function print5(){
-    alert(1);
-    alert(2);
-    alert(3);
-    alert(4);
-    alert(5);
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
 }
 
 print5();
