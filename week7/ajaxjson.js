4eb4bf459924c82349dfd419f255de620821c2cf
Tobi Abolade <tobiabolade@gmail.com>
2022-04-11 23:20:44 +0100

    wk done

---
+++ week7/ajaxjson.js
@@ -0,0 +1,36 @@
+function makeAjaxRequest(){
+    if (window.XMLHttpRequest){
+        xhr = new XMLHttpRequest();
+    }
+    else {
+        if (window.ActiveXObject){
+            xhr = newActiveXObject("Microsoft.XMLHTTP");
+        }
+
+    }
+
+    if (xhr) {
+        xhr.open("GET", "counties.json", true);
+        xhr.send();
+        xhr.onreadystatechange = showContents;
+    }
+    else {
+        document.getElementById("updatemessage").innerHTML = "Could not perform stated request"
+    }
+}
+
+function showContents(){
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200){
+            let data = JSON.parse(xhr.responseText);
+            let txt = "";
+            for (let i=0; i<data.counties.length; i++){
+                txt += "<tr><td>" + data.counties[i].name + "<td><tr>";
+            }
+            document.getElementById("countylist").innerHTML = txt;
+        } else {
+            document.getElementById("updatemessage").innerHTML = "An error occured: " + xhr.status;
+        }
+     }
+ }
+    
